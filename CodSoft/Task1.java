47ddc2b4fa79e51cad0cb1ee7a42120c408cb141
Rika Ghosh <rikaghosh23@gmail.com>
2023-09-30 08:48:59 +0530

    update

--- CodSoft/Task1.java
+++ CodSoft/Task1.java
@@ -1,42 +1,44 @@
 import java.util.*;
 
 class GuessNumber{
-    private int randomNumber;
-    private int numberOfAttempts = 0;
-    private int guessNumber;
+    public int randomNumber;
+    public int numberOfAttempts = 0;
+    public int playerGuess;
 
     //getter setter method for numberOfAttempts
-    public int getNumberOfAttempts(){
-        return numberOfAttempts;
-    }
     public void setNumberOfAttempts(int numberOfAttempts){
         this.numberOfAttempts = numberOfAttempts;
+    }
+    public int getNumberOfAttempts(){
+        return numberOfAttempts;
     }
 
     // creating a constructor to generate random number
     GuessNumber(){
+        int minNum = 1;
+        int maxNum = 100;
         Random r = new Random();
-        this.randomNumber = r.nextInt(101);
+        this.randomNumber = r.nextInt(maxNum - minNum + 1) + minNum;
     }
 
     //Method to take user input for the guessing number
     void takeInput(Scanner sc){
-        System.out.println("Guess any number: ");
-        guessNumber = sc.nextInt();
+        System.out.println("Guess any number(1-100): ");
+        playerGuess = sc.nextInt();
     }
 
     //Method to check if the gussing number is right or not
     boolean isCorrect(){
         numberOfAttempts++;
-        if (guessNumber == randomNumber){
+        if (playerGuess == randomNumber){
             System.out.println("Congratulations! Your Guess is Right.");
             System.out.println("You Gussed it in " + numberOfAttempts + " attempts.");
             return true;
         }
-        else if(guessNumber > randomNumber){
+        else if(playerGuess > randomNumber){
             System.out.println("Your Guessed number is too high!");
         }
-        else if(guessNumber < randomNumber){
+        else{
             System.out.println("Your Guessed number is too low!");
         }
         return false;
